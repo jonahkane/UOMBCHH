5b0ca0b75cec954d277dc39b69bc16e903746e6b
Jonah Kane <117034200+jonahkane@users.noreply.github.com>
2023-04-25 08:44:43 -0500

    Create login.js

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+//function used to log in. will direct the user to the dashboard page if success.
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (username && password) {
+      const response = await fetch('/api/users/login', {
+          method: 'post',
+          body: JSON.stringify({
+              username,
+              password
+          }),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      });
+
+      if (response.ok) {
+          document.location.replace('/dashboard');
+      } else {
+          alert(response.statusText);
+      }
+  }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
